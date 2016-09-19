2bd6ae65a28f7b78556172c05fdc64775b52bf36
vandanapl <vandanapl@gmail.com>
2016-09-19 10:12:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article_one',function(req,res){
+  res.send('Article one requested');  
+});
+
+app.get('/article_two',function(req,res){
+  res.send('Article two requested');  
+});
+
+app.get('/article_three',function(req,res){
+  res.send('Article three requested');  
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
