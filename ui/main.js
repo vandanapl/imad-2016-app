528ecfb6d2508d45700b425178f424d013487b94
vandanapl <vandanapl@gmail.com>
2016-09-21 11:42:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 var element = document.getElementById("newVal");
 element.innerHTML = "test";
 
+//move the image
+
+var img = document.getElementById("madi");
+img.onClick = function()
+{
+    img.style.marginleft = "100px"
+}
