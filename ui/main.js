4edc1f50dad3d62f681d563e5913594d45897dc2
vandanapl <vandanapl@gmail.com>
2016-09-21 14:45:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ function moveRight()
 
 img.onclick = function()
 {
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,50);
    // console.log('inside');
     //img.style.marginLeft = "100px";
 };
