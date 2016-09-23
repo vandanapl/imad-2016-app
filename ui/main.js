56b7da7b5b05216ceed696a5083fc9a342f32491
vandanapl <vandanapl@gmail.com>
2016-09-23 15:22:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ img.onclick = function()
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function() {
-    alert('hi');
+    
     var request = new XMLHttpRequest();
     
     request.onreadystatechange = function(){
@@ -34,6 +34,7 @@ button.onclick = function() {
             if(request.status === 200)
             {
                 var counter = request.responseText;
+                alert(counter.toString());
                  var span = document.getElementById('count');
     span.innerHTML = counter.toString();
             }
