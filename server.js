23bd571644a01bb2d2fcf592cad884cbf4b84526
vandanapl <vandanapl@gmail.com>
2016-09-23 10:07:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/', function (req, res) {
 var counter = 0;
 app.get('/counter',function(req,res){
     counter = counter + 1;
-    res.send(counter.tostring());
+    res.send(counter.toString());
 });
 /*
 app.get('/:articleName',function(req,res){
