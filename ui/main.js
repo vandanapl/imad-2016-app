5f73db73325a5f6985fb1ac6d6e8bf0392220a1c
vandanapl <vandanapl@gmail.com>
2016-09-23 11:48:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 var element = document.getElementById("newVal");
 element.innerHTML = "test";
 
@@ -19,4 +19,15 @@ img.onclick = function()
     var interval = setInterval(moveRight,50);
    // console.log('inside');
     //img.style.marginLeft = "100px";
+};*/
+
+
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function() {
+    counter = counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
 };
+ 
