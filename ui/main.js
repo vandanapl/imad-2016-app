5c491b3269b5c05db3f4e8920b99c85d58b8195a
vandanapl <vandanapl@gmail.com>
2016-09-23 15:57:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -56,7 +56,8 @@ var nameInput =document.getElementById('one');
 var nameInput = nameInput.value;
 var submit = document.getElementById('two');
 submit.onclick = function() {
-    var names =['name1','name2','name3'];
+    alert(hi)
+    var names =['name11','name22','name33'];
     var list ='';
     for(var i=0; i<names.length;i++){
         list += '<li>' +names[i] + '</li>';
